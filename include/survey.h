5a459b302d0ceccf8ff91f82d5a8c1ed04322d64
Pierce Doogan <pido3285@colorado.edu>
2016-04-14 14:52:22 -0600

    First Commit

---
+++ include/survey.h
@@ -0,0 +1,14 @@
+#ifndef SURVEY_H
+#define SURVEY_H
+
+
+class survey
+{
+    public:
+        survey();
+        virtual ~survey();
+    protected:
+    private:
+};
+
+#endif // SURVEY_H
---
+++ src/survey.cpp
@@ -0,0 +1,11 @@
+#include "survey.h"
+
+survey::survey()
+{
+    //ctor
+}
+
+survey::~survey()
+{
+    //dtor
+}
